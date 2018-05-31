9a91371eace4baa0950f6c0380e2b84979ae0936
skt-t1-byungi <tiniwz@gmail.com>
2018-05-31 18:01:11 +0900

    uncache -> vm2

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ const PluginError = require('plugin-error')
 const {resolve, basename, extname} = require('path')
 const {silent: resolveFrom} = require('resolve-from')
 const {Compile, parse} = require('velocityjs')
-const requireUncached = require('require-uncached')
 
 module.exports = function (ctx, opts = {}) {
   return through.obj((file, enc, cb) => {
     if (file.isNull()) return cb(null, file)
     if (file.isStream()) return cb(new PluginError('gulp-velocity.js', 'Streaming not supported!'))
 
-    let userData
-    if (typeof ctx === 'string') {
-      try {
-        userData = requireUncached(
-          resolveFrom(process.cwd(), ctx) ||
-          resolveFrom(process.cwd(), resolve(ctx, basename(file.path, extname(file.path))))
-        )
-      } catch (err) {
-        userData = {}
-      }
-    } else {
-      userData = Object.assign({}, ctx, file.data)
-    }
-
     try {
       const asts = parse(file.contents.toString(), opts.blocks, opts.ignorespace)
-      file.contents = Buffer.from((new Compile(asts)).render(userData, opts.macros))
+      const rendered = (new Compile(asts)).render(resolveUserData(ctx, file), opts.macros)
+
+      file.contents = Buffer.from(rendered)
     } catch (err) {
       return cb(new PluginError('gulp-velocity.js', err, {fileName: file.path}))
     }
 
     cb(null, file)
   })
+}
+
+function resolveUserData (ctx, file) {
+  if (typeof ctx !== 'string') return Object.assign({}, ctx, file.data)
+
+  try {
+    return require('vm2').NodeVM.file(
+      resolveFrom(process.cwd(), ctx) ||
+      resolveFrom(process.cwd(), resolve(ctx, basename(file.path, extname(file.path))))
+    )
+  } catch (err) {
+    return err
+  }
 }
