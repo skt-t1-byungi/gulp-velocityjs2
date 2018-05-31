9d450e258ec8c274c1cd8202b64c1d679f091d4e
Byungi <tiniwz@gmail.com>
2018-05-31 22:35:18 +0900

    refactor

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const {silent: resolveFrom} = require('resolve-from')
 const {Compile, parse} = require('velocityjs')
 const importFresh = require('import-fresh')
 
+const cwd = process.cwd()
+
 module.exports = function (ctx, opts = {}) {
   return through.obj((file, enc, cb) => {
     if (file.isNull()) return cb(null, file)
-    if (file.isStream()) return cb(new PluginError('gulp-velocity.js', 'Streaming not supported!'))
+
+    if (file.isStream()) {
+      const gulpErr = new PluginError('gulp-velocity.js', 'Streaming not supported!')
+      return cb(gulpErr)
+    }
 
     try {
       const asts = parse(file.contents.toString(), opts.blocks, opts.ignorespace)
-      const rendered = (new Compile(asts, opts)).render(resolveUserData(ctx, file), opts.macros)
+      const rendered = (new Compile(asts, opts))
+        .render(
+          resolveUserData(ctx, file),
+          opts.macros
+        )
 
       file.contents = Buffer.from(rendered)
     } catch (err) {
@@ -26,12 +36,15 @@ module.exports = function (ctx, opts = {}) {
 function resolveUserData (ctx, file) {
   if (typeof ctx !== 'string') return Object.assign({}, ctx, file.data)
 
+  let userData
   try {
-    return importFresh(
-      resolveFrom(process.cwd(), ctx) ||
-      resolveFrom(process.cwd(), resolve(ctx, basename(file.path, extname(file.path))))
+    userData = importFresh(
+      resolveFrom(cwd, ctx) ||
+      resolveFrom(cwd, resolve(ctx, basename(file.path, extname(file.path))))
     )
   } catch (err) {
-    return {}
+    userData = {}
   }
+
+  return Object.assign(userData, file.data)
 }
