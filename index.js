df87d995e16feae25e1b78a3cb32c63f6d476d60
skt-t1-byungi <tiniwz@gmail.com>
2019-06-10 00:24:29 +0900

    refactor format & cleanup

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
 const through = require('through2')
 const PluginError = require('plugin-error')
-const {resolve, basename, extname} = require('path')
-const {silent: resolveFrom} = require('resolve-from')
-const {Compile, parse} = require('velocityjs')
+const { resolve, basename, extname } = require('path')
+const { silent: resolveFrom } = require('resolve-from')
+const { Compile, parse } = require('velocityjs')
 const importFresh = require('import-fresh')
 
 const cwd = process.cwd()
 
-module.exports = function (ctx, opts = {}) {
-  return through.obj((file, enc, cb) => {
-    if (file.isNull()) return cb(null, file)
+module.exports = function (path, opts = {}) {
+    return through.obj((file, enc, cb) => {
+        if (file.isNull()) return cb(null, file)
 
-    if (file.isStream()) {
-      const gulpErr = new PluginError('gulp-velocity.js', 'Streaming not supported!')
-      return cb(gulpErr)
-    }
+        if (file.isStream()) {
+            return cb(new PluginError('gulp-velocity.js', 'Streaming not supported!'))
+        }
 
-    try {
-      const asts = parse(file.contents.toString(), opts.blocks, opts.ignorespace)
-      const rendered = (new Compile(asts, opts))
-        .render(
-          resolveUserData(ctx, file),
-          opts.macros
-        )
+        try {
+            const ast = parse(file.contents.toString(), opts.blocks, opts.ignorespace)
+            const rendered = (new Compile(ast, opts)).render(resolveUserData(path, file), opts.macros)
+            file.contents = Buffer.from(rendered)
+        } catch (err) {
+            return cb(new PluginError('gulp-velocity.js', err, { fileName: file.path }))
+        }
 
-      file.contents = Buffer.from(rendered)
-    } catch (err) {
-      return cb(new PluginError('gulp-velocity.js', err, {fileName: file.path}))
-    }
-
-    cb(null, file)
-  })
+        cb(null, file)
+    })
 }
 
-function resolveUserData (ctx, file) {
-  if (typeof ctx !== 'string') return Object.assign({}, ctx, file.data)
+function resolveUserData (path, file) {
+    if (typeof path !== 'string') return Object.assign({}, path, file.data)
 
-  let userData
-  try {
-    userData = importFresh(
-      resolveFrom(cwd, ctx) ||
-      resolveFrom(cwd, resolve(ctx, basename(file.path, extname(file.path))))
-    )
-  } catch (err) {
-    userData = {}
-  }
+    let userData
+    try {
+        userData = importFresh(
+            resolveFrom(cwd, path) ||
+      resolveFrom(cwd, resolve(path, basename(file.path, extname(file.path))))
+        )
+    } catch (err) {
+        userData = {}
+    }
 
-  return Object.assign(userData, file.data)
+    return Object.assign(userData, file.data)
 }
--- test.js
+++ test.js
@@ -3,21 +3,21 @@ import File from 'vinyl'
 import gulpVTL from '.'
 
 const fixture = (tpl, stream, opts = {}) => new Promise((resolve, reject) => {
-  stream.on('data', resolve)
-  stream.on('error', reject)
+    stream.on('data', resolve)
+    stream.on('error', reject)
 
-  stream.end(new File({
-    cwd: __dirname,
-    base: __dirname,
-    contents: Buffer.from(tpl),
-    ...opts
-  }))
+    stream.end(new File({
+        cwd: __dirname,
+        base: __dirname,
+        contents: Buffer.from(tpl),
+        ...opts
+    }))
 })
 
-test(async t => {
-  const file = await fixture(
-    'hello $value',
-    gulpVTL({value: 'world'})
-  )
-  t.is(file.contents.toString(), 'hello world')
+test('basic', async t => {
+    const file = await fixture(
+        'hello $value',
+        gulpVTL({ value: 'world' })
+    )
+    t.is(file.contents.toString(), 'hello world')
 })
