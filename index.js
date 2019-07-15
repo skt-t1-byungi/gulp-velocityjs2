d6b94d8582f5164eb1123bbab5e642e41c44b5c3
skt-t1-byungi <tiniwz@gmail.com>
2019-07-15 15:04:23 +0900

    refactor

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const { resolve, basename, extname } = require('path')
 const { silent: resolveFrom } = require('resolve-from')
 const { Compile, parse } = require('velocityjs')
 const importFresh = require('import-fresh')
+const replaceExt = require('replace-ext')
 
-const cwd = process.cwd()
-
-module.exports = function (path, opts = {}) {
+module.exports = function (data, opts = {}) {
     return through.obj((file, enc, cb) => {
         if (file.isNull()) return cb(null, file)
 
@@ -15,9 +14,28 @@ module.exports = function (path, opts = {}) {
             return cb(new PluginError('gulp-velocity.js', 'Streaming not supported!'))
         }
 
+        if (typeof data === 'string') {
+            const cwd = process.cwd()
+            const load = opts.refresh ? importFresh : require
+            try {
+                data = load(
+                    resolveFrom(cwd, data) ||
+                    resolveFrom(cwd, resolve(data, basename(file.path, extname(file.path))))
+                )
+            } catch (err) {
+                data = {}
+            }
+        } else {
+            data = Object.assign(Object(data), file.data)
+        }
+
+        if (file.path) {
+            file.path = replaceExt(file.path, opts.ext === '' ? '' : '.' + (opts.ext || 'html'))
+        }
+
         try {
             const ast = parse(file.contents.toString(), opts.blocks, opts.ignorespace)
-            const rendered = (new Compile(ast, opts)).render(resolveUserData(path, file), opts.macros)
+            const rendered = (new Compile(ast, opts)).render(data, opts.macros)
             file.contents = Buffer.from(rendered)
         } catch (err) {
             return cb(new PluginError('gulp-velocity.js', err, { fileName: file.path }))
@@ -25,20 +43,4 @@ module.exports = function (path, opts = {}) {
 
         cb(null, file)
     })
-}
-
-function resolveUserData (path, file) {
-    if (typeof path !== 'string') return Object.assign({}, path, file.data)
-
-    let userData
-    try {
-        userData = importFresh(
-            resolveFrom(cwd, path) ||
-            resolveFrom(cwd, resolve(path, basename(file.path, extname(file.path))))
-        )
-    } catch (err) {
-        userData = {}
-    }
-
-    return Object.assign(userData, file.data)
 }
